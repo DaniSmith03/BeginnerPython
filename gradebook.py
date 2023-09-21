2fc7e8028b3ac98997d508bd2b3c53a87d339621
Dani Smith <danismith03@gmail.com>
2023-09-21 09:37:41 -0500

    list

---
+++ gradebook.py
@@ -0,0 +1,19 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+
+subjects=["physics", "calculus", "poetry", "history"]
+grades=[98,97,85,88]
+grade_book=[["physics", 98],["calculus", 97],["poetry", 85],["history", 88]]
+print(grade_book)
+grade_book.append(["computer science", 100])
+grade_book.append(["visual arts", 93])
+print(grade_book)
+grade_book[-1][-1]=grade_book[-1][-1]+5
+grade_book[2].remove(85)
+grade_book[2].append("Pass")
+
+print(grade_book)
+
+full_gradebook= grade_book+last_semester_gradebook
+print(full_gradebook)
